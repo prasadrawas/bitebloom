eba3007977ff11745391f2a7c80cf126f67262e9
Prasad Rawas <prasad.rawas@volox.com>
2026-06-02 22:19:51 +0530

    Fix summary provider to use selected date instead of hardcoded today

--- lib/providers/summary_provider.dart
+++ lib/providers/summary_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../data/models/daily_summary.dart';
 import '../core/utils/date_utils.dart';
 import 'auth_provider.dart';
+import 'meals_provider.dart';
 import 'profile_provider.dart';
 
 final dailySummaryProvider = StreamProvider<DailySummary?>((ref) {
@@ -14,7 +15,7 @@ final dailySummaryProvider = StreamProvider<DailySummary?>((ref) {
 final selectedDateSummaryProvider = StreamProvider<DailySummary?>((ref) {
   final user = ref.watch(currentUserProvider);
   if (user == null) return Stream.value(null);
-  // For diary screen we could use a separate date provider if needed
-  final today = AppDateUtils.todayKey();
-  return ref.watch(firestoreServiceProvider).streamDailySummary(user.uid, today);
+  final selectedDate = ref.watch(selectedDateProvider);
+  final dateKey = AppDateUtils.formatDate(selectedDate);
+  return ref.watch(firestoreServiceProvider).streamDailySummary(user.uid, dateKey);
 });
